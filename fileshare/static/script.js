ae62e93a6bf83bf8360b7d08a65485c2e66413f3
DevER-M <sankam.mithunganeshviiv@gmail.com>
2024-06-04 13:08:22 +0530

    flash fade out animation yay

--- fileshare/static/script.js
+++ fileshare/static/script.js
@@ -1,3 +1,3 @@
-window.setTimeout(function() {
-    $(".alert").fadeTo(500, 0) 
-}, 4000);
+var s = document.getElementById('hide').style;
+s.opacity = 1;
+(function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,200)})()
