0481d066b3ce37b36a984873c0f3c3c5938c16d3
DevER-M <sankam.mithunganeshviiv@gmail.com>
2024-07-27 20:37:33 +0530

    now??

--- api/run.py
+++ api/run.py
@@ -1,4 +1,7 @@
+import sys
+sys.path.insert(0,"../")
 from fileshare import app
+
 
 if __name__ == "__main__":
     app.run()
--- fileshare/__init__.py
+++ fileshare/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from sqlalchemy.orm import DeclarativeBase
 from flask_login import LoginManager
+import os
 
 
 class Base(DeclarativeBase):
@@ -17,7 +18,7 @@ login_manager = LoginManager()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SECRET_KEY"] = "iuhe398h432u8hnf401fhni32"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sql.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("sqlurl")
 
 db.init_app(app)
 login_manager.init_app(app)
